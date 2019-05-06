71cd751c08344ba801c794a4a9c2398af98a6fd7
Marcus Puie Vai Kwan <marcus@Marcuss-MacBook-Pro.local>
2019-05-07 06:22:26 +1000

    finished functions

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,60 @@
 // };
 // myFunction();
 
+// arrow syntax 
+const myFunction = () => {
+    console.log("Function was invoked!");
+};
+myFunction();
+
+/* streamlined arrow syntax 
+ const myFunction = () => console.log("Function was invoked!");
+*/
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+// arrow syntax 
+const anotherFunction = (param) => {
+    return param;
+};
+anotherFunction("Example");
+
+/* steamlined arrow function
+const anotherFunction = param => param;
+*/
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+// arrow syntax 
+const add = (param1, param2) => {
+    return param1 + param2;
+};
+add(1,2);
+
+/* streamlined arrow syntax
+const add = (param1, param2) => param1 + param2;
+*/
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+// arrow syntax 
+const subtract = (param1, param2) => {
+    return param1 - param2;
+}; 
+subtract(1,2);
+
+/* streamlined arrow syntax 
+const subtract = (param1, param2) => param1 - param2;
+*/
 
 // Stretch
 
