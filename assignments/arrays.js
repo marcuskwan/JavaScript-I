ee586ded4e1d3ac216deaec8b54cf099f8dd2463
Marcus Puie Vai Kwan <marcus@Marcuss-MacBook-Pro.local>
2019-05-07 06:14:33 +1000

    completed arrays

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -63,32 +63,80 @@ let inventory = [{"id":1,"car_make":"Lincoln","car_model":"Navigator","car_year"
 
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
-console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
+// actual logging substituting in strings
+console.log(`Car 33 is a *2011* *Jeep* *Wrangler*`);
+// creating variables for year,make,model
+let car33year = '';
+let car33make = '';
+let car33model = '';
+// creating for loop that updates values when id === 33. 
+for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i]['id'] === 33) {
+        car33year = inventory[i]['car_year'];
+        car33make = inventory[i]['car_make'];
+        car33model = inventory[i]['car_model'];
+        // logs sentence 
+        console.log(`Car 33 is a ` + car33year + ' ' + car33make + ' ' + car33model + '.');
+    }
+}
 
 // ==== Challenge 2 ====
 // The dealer needs the information on the last car in their inventory.  What is the make and model of the last car in the inventory?  Log the make and model into the console.
+// assigning lastCar to last item in the array
 let lastCar = 0;
-console.log();
+lastCar = inventory.length-1;
+// logging via bracket notation
+console.log("Last Car's make: " + inventory[lastCar]['car_make'] + ". Last Car's model: " + inventory[lastCar]['car_model']);
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
+// created carModels, assigned to empty array
 let carModels = [];
-console.log();
+// creating for loop that loops the array and pushes each object's car model to carModels
+for (let i = 0; i < inventory.length; i++) {
+    carModels.push(inventory[i]['car_model']);
+}
+// sorting carModels array alphabetically
+carModels.sort();
+// logging carModels
+console.log(carModels);
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
+// created carYears, assigned to empty array
 let carYears = [];
-console.log();
+// created for loop that loops throughout array and pushes each object's car year to carYears
+for (let i = 0; i < inventory.length; i++) {
+    carYears.push(inventory[i]['car_year']);
+}
+// logging carYears
+console.log(carYears);
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
+// created oldCars, assigned to empty array
 let oldCars = [];
-console.log(); 
+// creating for loop that loops throughout array and pushes the ID of the cars whose years are older than year 2000
+for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i]['car_year'] > 2000) {
+        oldCars.push(inventory[i].id);
+    }
+}
+// logging oldCars's length
+console.log(oldCars.length); 
 
 // ==== Challenge 6 ====
 // A buyer is interested in seeing only BMW and Audi cars within the inventory.  Return an array that only contains BMW and Audi cars.  Once you have populated the BMWAndAudi array, use JSON.stringify() to show the results of the array in the console.
+// created BMWAndAudi, assigned to empty array
 let BMWAndAudi = [];
-console.log();
+// creating for loop that loops throughout inventory array and pushes the models of BMW and Audi cars to BMWAndAudi array.
+for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i]['car_make'] === 'BMW' || inventory[i]['car_make'] === 'Audi') {
+        BMWAndAudi.push(inventory[i]['car_model']);
+    }
+}
+// using JSON.stringify method. I think this makes the elements into strings? read up on it, but don't quite see what's the point.
+JSON.stringify(BMWAndAudi);
 
-
-
+// logging BMWAndAudi 
+console.log(BMWAndAudi);
